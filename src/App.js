f1e69e37bfb6d16d870d3dddb3b8c5662233effd
Yaroslav <sirenko.it@gmail.com>
2023-05-11 12:21:04 +0300

    Lesson_61_cookie_login_auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Settingss from './components/Settingss/Settingss';
 import News from './components/News/News';
@@ -10,17 +9,18 @@ import Music from './components/Music/Music';
 import UsersContainer from './components/Users/UsersContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import ProfileContainer from './components/Profile/ProfileInfo/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
-        <Header />
+        <HeaderContainer />
         <Navbar state={props.name}/>
         <div className="app-wrapper-content">
           <Routes>
-            <Route path='/profile' element={<ProfileContainer />} />
+            <Route path='/profile/:userId?' element={<ProfileContainer />} />
             <Route path='/dialogs' element={<DialogsContainer />} /> 
             <Route path='/music' element={<Music />} />
             <Route path='/news' element={<News />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://cdn.worldvectorlogo.com/logos/bmw-logo-gray-.svg' alt='bmw'></img>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login
+                    : <NavLink to = {'/login'}>Login</NavLink> }
+            </div>
         </header>
     )
 }

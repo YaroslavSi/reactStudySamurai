070097e4e1d1a4f72a78b834529c3d541072e546
Yaroslav <sirenko.it@gmail.com>
2023-05-12 17:24:24 +0300

    Lesson_65_button_disabled

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './users.module.css';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 
 
@@ -32,9 +33,38 @@ let Users = (props) => {
                         </div>
                         <div>
                             { u.followed 
-                                ? <button onClick={() => {props.unfollow(u.id)}}>UnFollow</button> 
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
-                            
+                                
+                                ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                    props.toggleFollowingProgress(true, u.id);
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                        withCredentials: true,
+                                        // headers: {
+                                        //     "API-KEY": "b777-"
+                                        // }
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode == 0) {
+                                                props.unfollow(u.id);
+                                            }
+                                            props.toggleFollowingProgress(false, u.id);
+                                        })
+                                }}>UnFollow</button> 
+
+                                : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                    props.toggleFollowingProgress(true, u.id);
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true,
+                                        // headers: {
+                                        //     "API-KEY": "b777-"
+                                        // }
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode == 0) {
+                                                props.follow(u.id);
+                                            }
+                                            props.followingInProgress(false, u.id);
+                                        }) 
+                                }}>Follow</button> }
                         </div>
                     </span>
                     <span>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,6 +4,8 @@ import { follow, setCurrentPage, setUsers, setUsersTotalCount, toggleIsFetching,
 import axios from 'axios';
 import Users from './Users';
 import Preloader from '../common/Preloader/Prealoader';
+import { usersAPI } from '../../api/api';
+import { toggleFollowingProgress } from '../../redux/users-reducer';
 
 class UsersContainer extends React.Component {
 
@@ -13,21 +15,22 @@ class UsersContainer extends React.Component {
 
     componentDidMount() {
         // this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
+            .then(data => {
                 // this.props.toggleIsFetching(false);
-                this.props.setUsers (response.data.items);
-                this.props.setTotalUsersCount (response.data.totalCount);
-            
+                this.props.setUsers (data.items);
+                this.props.setTotalUsersCount (data.totalCount);
             });
     }
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-        .then(response => {
-            this.props.setUsers (response.data.items);
-        
-        });
+
+        usersAPI.getUsers(pageNumber, this.props.pageSize)
+            .then(data => {
+                this.props.setUsers (data.items);
+            });
     }
 
     render() {
@@ -40,6 +43,8 @@ class UsersContainer extends React.Component {
                 users = {this.props.users}
                 follow = {this.props.follow}
                 unfollow = {this.props.unfollow}
+                toggleFollowingProgress = {this.props.toggleFollowingProgress}
+                followingInProgress = {this.props.followingInProgress}
             />
         </>
     }
@@ -51,7 +56,8 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.pageSize,
         currentPage: state.usersPage.currentPage,
-        ifFetching: state.usersPage.isFetching
+        ifFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.toggleFollowingProgress,
     }
 };
 // let mapDispatchToProps = (dispatch) => {
@@ -78,5 +84,5 @@ let mapStateToProps = (state) => {
 // }
 
 export default connect (mapStateToProps, 
-    { follow, unfollow, setUsers, setCurrentPage, setUsersTotalCount, toggleIsFetching
+    { follow, unfollow, setUsers, setCurrentPage, setUsersTotalCount, toggleIsFetching, toggleFollowingProgress
     }) (UsersContainer);
